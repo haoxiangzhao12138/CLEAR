44558e6de092b4b607ce0c8526c16690a94a2f42
haoxiangzhao12138 <haoxiangzhao2023@ia.ac.cn>
2026-04-11 23:19:14 +0800

    share debug

--- VLMEvalKit/eval.sh
+++ VLMEvalKit/eval.sh
@@ -3,6 +3,9 @@
 # 记录开始时间
 start_time=$(date +%s)
 
+# 设置 LMUData 数据路径
+export LMUData=/root/paddlejob/gpfsspace/haoxiangzhao/CLEAR/datasets/LMUData
+
 # 说明：
 # 1. 移除了 --verbose 后面的反斜杠
 # 2. 只有 MME 数据集时，不需要 --judge 参数，MME 是规则评测
@@ -10,7 +13,7 @@ torchrun \
     --nproc-per-node=8 \
     --master_port=29503 \
     run.py \
-    --config ./config/gpt4_1_eval.json \
+    --config ./config/CV-Bench-2D_per_degradation.json \
     --judge gpt-4-0125 \
     --verbose
 
